07621ce21c70230b5795ed2b95cd6a76f3dbdc86
TGLEEEE <itstaegil@gmail.com>
2023-04-12 19:21:04 +0900

    Feat: PuzzleRoomOneTouchPad 추가

---
+++ Source/SpaceEscape/Public/PuzzleRoomOneTouchPad.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PuzzleBase.h"
+#include "PuzzleRoomOneTouchPad.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEESCAPE_API APuzzleRoomOneTouchPad : public APuzzleBase
+{
+	GENERATED_BODY()
+	
+};
