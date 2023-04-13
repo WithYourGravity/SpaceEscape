5b373f9c030e19d1a6ecdabecfdd36d9d174b86b
TGLEEEE <itstaegil@gmail.com>
2023-04-13 15:03:51 +0900

    Feat: 추가 PuzzleRoomOneTouchPadPanelWidget
    터치패드의 화면부분을 출력할 위젯 추가

---
+++ Source/SpaceEscape/Public/PuzzleRoomOneTouchPadPanelWidget.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PuzzleRoomOneTouchPadPanelWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEESCAPE_API UPuzzleRoomOneTouchPadPanelWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly)
+	class UTextBlock* TextBlock_Number;
+
+	FString GetCurrentScreen();
+	void deleteCurrentScreen();
+	void addCurrentScreen(FString number);
+
+private:
+	FString currentScreen;
+};
