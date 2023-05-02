a9f5912cb632220bc5398fd22b3f2237417700e2
TGLEEEE <itstaegil@gmail.com>
2023-05-02 16:52:33 +0900

    Fix: 버그 수정 (Prototype)
    Prototype Ver.

--- Source/SpaceEscape/Private/EscapePlayer.cpp
+++ Source/SpaceEscape/Private/EscapePlayer.cpp
@@ -280,9 +280,8 @@ bool AEscapePlayer::CheckHitTeleport(FVector lastPos, FVector& curPos)
 	// 두 점 사이에 충돌체 있는지 확인
 	FHitResult hitInfo;
 	bool bHit = HitTest(lastPos, curPos, hitInfo);
-	
 	// 만약 부딪힌 대상이 바닥이라면
-	if (bHit && hitInfo.GetActor()->GetActorLabel().Contains(TEXT("Floor")))
+	if (bHit && hitInfo.GetActor()->GetActorNameOrLabel().Contains(TEXT("Floor")))
 	{
 		// endPos 를 부딪힌 곳으로 수정
 		curPos = hitInfo.Location;
@@ -295,8 +294,7 @@ bool AEscapePlayer::CheckHitTeleport(FVector lastPos, FVector& curPos)
 	else
 	{
 		teleportCircle->SetVisibility(false);
-	}
-
+	} 
 	return bHit;
 }
 
