ee7f57ef403372272bfcb4b06bc1597e5e0060ba
TGLEEEE <itstaegil@gmail.com>
2023-05-19 13:58:37 +0900

    Feat: 추가 SpaceShip
    탈출용 SpaceShip 추가 구현

---
+++ Source/SpaceEscape/Public/SpaceShip.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpaceShip.generated.h"
+
+UCLASS()
+class SPACEESCAPE_API ASpaceShip : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASpaceShip();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UStaticMeshComponent* shipMeshComp;
+	UPROPERTY(EditDefaultsOnly)
+	class UBoxComponent* boxComp;
+	UPROPERTY(EditDefaultsOnly)
+	class USphereComponent* forLocComp;
+
+	UFUNCTION()
+	void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	void BoardingShip();
+
+private:
+	UPROPERTY()
+	class AEscapePlayer* player;
+	EMoveModeState tempSaveMoveMode;
+	bool bIsBoarding;
+	bool bReadyToBoarding;
+
+};
