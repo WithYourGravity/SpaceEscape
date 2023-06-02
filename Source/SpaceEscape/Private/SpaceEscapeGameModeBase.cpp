bbc4e7ad950f946d8de2f233d61450ae832830b0
TGLEEEE <itstaegil@gmail.com>
2023-06-02 20:00:48 +0900

    Feat: 추가 StageCheat
    게임모드 SpaceEscapeGameModeBase에 해당 스테이지로 바로 이동하게 해주는 함수 구현, 플레이어 입력과 연결해야 함

--- Source/SpaceEscape/Private/SpaceEscapeGameModeBase.cpp
+++ Source/SpaceEscape/Private/SpaceEscapeGameModeBase.cpp
@@ -4,6 +4,7 @@
 #include "SpaceEscapeGameModeBase.h"
 #include "EscapePlayer.h"
 #include "PlayerInfoWidget.h"
+#include "RoomManager.h"
 #include "Kismet/GameplayStatics.h"
 
 void ASpaceEscapeGameModeBase::BeginPlay()
@@ -11,8 +12,10 @@ void ASpaceEscapeGameModeBase::BeginPlay()
 	Super::BeginPlay();
 
 	player = Cast<AEscapePlayer>(UGameplayStatics::GetPlayerCharacter(GetWorld(), 0));
+	rm = Cast<ARoomManager>(UGameplayStatics::GetActorOfClass(this, ARoomManager::StaticClass()));
 }
 
+// 플레이타임 카운트하는 함수
 void ASpaceEscapeGameModeBase::CountPlayTime()
 {
 	second++;
@@ -29,12 +32,41 @@ void ASpaceEscapeGameModeBase::CountPlayTime()
 	}
 }
 
+// 플레이타임 카운트를 멈추는 함수
 void ASpaceEscapeGameModeBase::StopPlayTime()
 {
 	GetWorld()->GetTimerManager().ClearTimer(countTimer);
 }
 
+// 플레이타임 카운트를 시작하는 함수
 void ASpaceEscapeGameModeBase::StartPlayTime()
 {
 	GetWorld()->GetTimerManager().SetTimer(countTimer, this, &ASpaceEscapeGameModeBase::CountPlayTime, 1.0f, true, 0);
+}
+
+// 스테이지를 입력하면 바로 그 스테이지부터 시작하게 해주는 함수
+void ASpaceEscapeGameModeBase::SetStage(int stage)
+{
+	UGameplayStatics::OpenLevel(this, FName("FinalMap"));
+	rm->MoveOnNextStage();
+	StartPlayTime();
+
+	switch (stage)
+	{
+	case 1 :
+		player->SetActorLocation(FVector(-6100, -2430, 80));
+		break;
+	case 2:
+		rm->MoveOnNextStage();
+		rm->MoveOnNextStage();
+		player->SetActorLocationAndRotation(FVector(-3500, -3520, 130), FRotator(0, -90, 0));
+		break;
+	case 3:
+		rm->MoveOnNextStage();
+		rm->MoveOnNextStage();
+		rm->MoveOnNextStage();
+		rm->MoveOnNextStage();
+		player->SetActorLocationAndRotation(FVector(-3415, -600, 1350), FRotator(0, 90, 0));
+		break;
+	}
 }
