9d11c905f2b8d942e047b0839b7df5bb9a363d52
yeni0224 <yeniyeni0224@gmail.com>
2023-07-13 21:49:56 +0900

    Feat : 수정 오디오, Light, MovableObj, 프로토콜
    라디오, 우주선 sound 추가 및 범위 수정
    우주선으로 향하는 통로 빛 범위 수정
    Room 1 : 물건을 잡고 던질 수 있는 물체로 수정
    비상 프로토콜 문구 및 배치 수정

---
+++ Source/SpaceEscape/Private/interactiveObj.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "interactiveObj.h"
+#include "Components/BoxComponent.h"
+#include "GrabComponent.h"
+
+// Sets default values
+AinteractiveObj::AinteractiveObj()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	boxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("boxComp"));
+	SetRootComponent(boxComp);
+	meshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("meshComp"));
+	meshComp->SetupAttachment(RootComponent);
+	grabComp = CreateDefaultSubobject<UGrabComponent>(TEXT("GrabComp"));
+	grabComp->SetupAttachment(RootComponent);
+	grabComp->grabType = EGrabType::FREE;
+}
+
+// Called when the game starts or when spawned
+void AinteractiveObj::BeginPlay()
+{
+	Super::BeginPlay();
+
+	boxComp->SetSimulatePhysics(true);
+}
+
+// Called every frame
+void AinteractiveObj::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
